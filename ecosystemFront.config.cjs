ae189a6890ce3ea0efde30daed4c5565c748f8c7
Enzo Sives <enzo.sives@telpin.com.ar>
2025-10-28 12:29:53 -0300

    Config Pm2

--- ecosystemFront.config.cjs
+++ ecosystemFront.config.cjs
@@ -1,13 +1,17 @@
+// ecosystemFront.config.cjs
 module.exports = {
   apps: [
     {
-      name: "Front",
-      script: "npm",
-      args: "run start:pwa",
-      watch: true, // reiniciar automáticamente la aplicación al cambiar los archivos
-      env: {
-        NODE_ENV: "development",
-      },
+      name: "front-historias",
+      script: "serve", // El comando que PM2 debe ejecutar
+      args: [
+        "-s", // Argumento 1: modo SPA
+        "dist/pwa", // Argumento 2: la carpeta
+        "-l", // Argumento 3: la bandera de 'listen'
+        "tcp://127.0.0.1:3000", // Argumento 4: la dirección y puerto
+      ],
+      // Importante: le dice a PM2 dónde ejecutar el comando
+      cwd: "/home/hcmadariaga.online/public_html/front-historias-clinicas",
     },
   ],
 };
