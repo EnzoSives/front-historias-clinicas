8eb04f8c9a7522bbf7ff02a36e0cf7bf9e62697d
Enzo Sives <enzo.sives@telpin.com.ar>
2025-10-28 12:31:34 -0300

    fix

--- ecosystemFront.config.cjs
+++ ecosystemFront.config.cjs
@@ -3,14 +3,16 @@ module.exports = {
   apps: [
     {
       name: "front-historias",
-      script: "serve", // El comando que PM2 debe ejecutar
+      script: "serve",
+
+      // Argumentos en el orden correcto: Banderas primero, ruta al final.
       args: [
-        "-s", // Argumento 1: modo SPA
-        "dist/pwa", // Argumento 2: la carpeta
-        "-l", // Argumento 3: la bandera de 'listen'
-        "tcp://127.0.0.1:3000", // Argumento 4: la dirección y puerto
+        "-s",
+        "-l",
+        "tcp://127.0.0.1:3000",
+        "dist/pwa", // <--- La ruta va al final
       ],
-      // Importante: le dice a PM2 dónde ejecutar el comando
+
       cwd: "/home/hcmadariaga.online/public_html/front-historias-clinicas",
     },
   ],
