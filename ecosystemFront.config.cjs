fff2e2e2020389bea3ba8d6db49d1815837ddc09
Enzo Sives <enzo.sives@telpin.com.ar>
2025-10-28 12:35:50 -0300

    Vuelvo para atras

--- ecosystemFront.config.cjs
+++ ecosystemFront.config.cjs
@@ -1,19 +1,13 @@
-// ecosystemFront.config.cjs
 module.exports = {
   apps: [
     {
-      name: "front-historias",
-      script: "serve",
-
-      // Argumentos en el orden correcto: Banderas primero, ruta al final.
-      args: [
-        "-s",
-        "-l",
-        "tcp://127.0.0.1:3000",
-        "dist/pwa", // <--- La ruta va al final
-      ],
-
-      cwd: "/home/hcmadariaga.online/public_html/front-historias-clinicas",
+      name: "Front",
+      script: "npm",
+      args: "run start:pwa",
+      watch: true, // reiniciar automáticamente la aplicación al cambiar los archivos
+      env: {
+        NODE_ENV: "development",
+      },
     },
   ],
 };
