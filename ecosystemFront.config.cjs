79b104dd73249b8d6e0ed1d90c0f4dc716ab302a
Enzo Sives <enzo.sives@telpin.com.ar>
2025-10-28 11:38:29 -0300

    Faltaba run

--- ecosystemFront.config.cjs
+++ ecosystemFront.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "Front",
       script: "npm",
-      args: "start:pwa",
+      args: "run start:pwa",
       watch: true, // reiniciar automáticamente la aplicación al cambiar los archivos
       env: {
         NODE_ENV: "development",
